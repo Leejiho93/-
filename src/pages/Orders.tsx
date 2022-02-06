d06decc969d1a650338e26eb23476f360dd230a1
leejiho <easyho93@gmail.com>
2022-02-07 01:05:18 +0900

    우와요기이츠 클론

---
+++ src/pages/Orders.tsx
@@ -0,0 +1,24 @@
+import React, {useCallback} from 'react';
+import {View, FlatList} from 'react-native';
+import {useSelector} from 'react-redux';
+import EachOrder from '../components/EachOrder';
+import {Order} from '../slices/order';
+import {RootState} from '../store/reducer';
+
+function Orders() {
+  const orders = useSelector((state: RootState) => state.order.orders);
+  const renderItem = useCallback(({item}: {item: Order}) => {
+    return <EachOrder item={item} />;
+  }, []);
+  return (
+    <View>
+      <FlatList
+        data={orders}
+        keyExtractor={item => item.orderId}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+}
+
+export default Orders;
